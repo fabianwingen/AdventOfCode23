2588fd818c2f7ccda8118bfd3284c6ea06112c01
Fabian Wingen <fabian.wingen@hhu.de>
2023-12-02 01:03:37 +0100

    add Day2 Part2

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -4,16 +4,17 @@ fun main() {
     val path = "src/main/resources/input1.txt"
     val file = File(path)
     part1(file)
+    part2(file)
 }
 
 fun part1(file: File) {
     var summe = 0
     file.forEachLine { line ->
-        summe += getNumber(line)
+        summe += getDigits(line)
     }
-    print(summe)
+    println("Part1: " +summe)
 }
-fun getNumber(line: String): Int {
+fun getDigits(line: String): Int {
     var chars: String = ""
     for (c in line) {
         if ( c.isDigit()) {
@@ -24,3 +25,35 @@ fun getNumber(line: String): Int {
     return number.toInt()
 
 }
+fun part2(file: File) {
+    var summe = 0
+    file.forEachLine { line ->
+        summe += getSum(line)
+    }
+    print("Part2: " + summe)
+}
+fun getSum(line: String): Int {
+    val textToDigit = mapOf(
+        "one" to "1", "two" to "2", "three" to "3", "four" to "4", "five" to "5", "six" to "6",
+        "seven" to "7", "eight" to "8", "nine" to "9", "1" to "1", "2" to "2", "3" to "3",
+        "4" to "4", "5" to "5", "6" to "6", "7" to "7", "8" to "8", "9" to "9")
+    val frontSubstrings = mutableListOf<String>()
+    for (i in 1..line.length) {
+        frontSubstrings.add(line.substring(0,i));
+    }
+
+    val backSubstrings = mutableListOf<String>()
+    for (i in 1..line.length) {
+        backSubstrings.add(line.substring(line.length - i))
+    }
+
+    val firstSubstringContainingADigit = frontSubstrings.find { substring -> textToDigit.keys.any { substring.contains(it)} }
+    val lastSubstringContainingADigit = backSubstrings.find { substring -> textToDigit.keys.any{ substring.contains(it)} }
+    val firstDigit = textToDigit.keys.find { firstSubstringContainingADigit!!.contains(it) }
+    val lastDigit = textToDigit.keys.find { lastSubstringContainingADigit!!.contains(it) }
+
+    val summe = "" + textToDigit.get(firstDigit) + textToDigit.get(lastDigit)
+    return summe.toInt()
+}
+
+
